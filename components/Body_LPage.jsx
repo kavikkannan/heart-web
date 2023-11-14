03d2b4ab6306132e380a50d65dd6769b3642aa9c
subi <rsubikksha@gmail.com>
2023-11-15 01:01:10 +0530

    try1

---
+++ components/Body_LPage.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+const HeartPrediction = () => {
+  return (
+    <div className="bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 py-16">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 className="text-4xl font-extrabold tracking-tight text-black sm:text-5xl">
+          Predicting Heart Disease
+        </h2>
+        <p className="mt-4 max-w-3xl text-lg text-black">
+          Heart disease prediction made easy. Check your risk factors and take control of your health.
+        </p>
+        <div className="mt-10">
+          <h3 className="text-2xl font-bold mb-4 text-black">Key Factors Considered:</h3>
+          <ul className="list-disc list-inside text-black">
+            <li>Cholesterol levels</li>
+            <li>Blood pressure</li>
+            <li>Family history</li>
+            <li>Physical activity</li>
+            <li>Age and gender</li>
+          </ul>
+        </div>
+        <div className="mt-10">
+          <p className="text-lg text-black">
+            Our advanced machine learning algorithms analyze various health parameters and historical data to assess the risk of heart disease.
+            Input your details for a preliminary assessment.
+          </p>
+          <button className="mt-6 inline-flex items-center px-8 py-3 border border-transparent text-lg font-medium rounded-md text-black bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+            Check Your Risk
+          </button>
+        </div>
+        <div className="mt-16">
+          <h3 className="text-3xl font-bold mb-6 text-black">Preventive Measures:</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <div className="bg-black rounded-lg p-6">
+              <h4 className="text-lg font-semibold mb-4 text-blue-500">Maintain a Healthy Diet</h4>
+              <p className="text-blue-300">
+                Consume more fruits, vegetables, and whole grains. Limit saturated fats and sugars.
+              </p>
+            </div>
+            <div className="bg-black rounded-lg p-6">
+              <h4 className="text-lg font-semibold mb-4 text-blue-500">Regular Exercise</h4>
+              <p className="text-blue-300">
+                Engage in at least 30 minutes of moderate exercise most days of the week.
+              </p>
+            </div>
+            <div className="bg-black rounded-lg p-6">
+              <h4 className="text-lg font-semibold mb-4 text-blue-500">Manage Stress</h4>
+              <p className="text-blue-300">
+                Practice relaxation techniques like meditation or yoga to reduce stress levels.
+              </p>
+            </div>
+            <div className="bg-black rounded-lg p-6">
+              <h4 className="text-lg font-semibold mb-4 text-blue-500">Regular Check-ups</h4>
+              <p className="text-blue-300">
+                Visit your healthcare provider for routine check-ups and screenings.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeartPrediction;
---
+++ components/Login.jsx
@@ -0,0 +1,267 @@
+'use client'
+import {useRouter} from "next/navigation"
+import Link from "next/link"
+import React, { useState } from "react"; 
+import Heart from '@/components/Body_LPage';
+import { Result } from "postcss";
+export default function Login() {
+  const [loading, setLoading] = useState(false);
+  const router=useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [data, setData] = useState([]);
+  const [darkMode, setDarkMode] = useState(false); 
+
+
+  const toggleMode = () => {
+    setDarkMode(!darkMode);
+  };
+  const Signin = async () => {
+    try {
+
+      setLoading(true);
+  
+      const log = await fetch(`http://localhost:9000/api/login`, {
+        method: "POST",
+        mode:"cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        credentials: 'include',
+      })
+      
+        const response = await log.json();
+        if(response.message=='success'){
+          router.push('#');
+          setLoading(false);
+        }
+        else{
+          setLoading(false);
+          alert(response.message)
+        }
+      
+
+      
+    } catch (error) {
+      console.error(error);
+    } /* finally {
+      setLoading(false);
+    } */
+    
+  };   
+
+
+    return (
+      <>
+      {darkMode ? (
+          <div
+          className={`absolute w-full bg-black flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8`}
+        >
+          <div className="absolute ">
+          <center>
+          <img
+                className=" text-blue-200 "
+                src="/images/heartD.jpg"
+        
+              /></center>
+          </div>
+         
+          
+          <div className="fixed top-4 right-4 z-50">
+              <button
+                onClick={toggleMode}
+                className="px-4 py-2 bg-gray-300 rounded-md"
+              >
+                {darkMode ? 'Light Mode' : 'Dark Mode'}
+              </button>
+            </div>
+            <div className=" absolute top-10 right-13 sm:mx-auto sm:w-full sm:max-w-sm">
+            <Link href="/"><img
+                className=" text-blue-200 mx-auto h-10 w-auto"
+                src=""
+                alt="Your Company"
+              />
+              </Link>
+            </div>
+            
+            <div className=" relative bottom-20 sm:mx-auto sm:w-full sm:max-w-sm">
+              
+              <h2 className=" mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-blue-300 ">
+                Sign in  account
+              </h2>
+            </div>
+    
+            <div className="relative bottom-10 mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="email" className="block text-blue-200 text-sm font-medium leading-6 ">
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="block w-full rounded-md p-2 border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-blue-200 placeholder:text-blue-200 focus:ring-2 focus:ring-inset focus:ring-blue-200 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-blue-200">
+                      Password
+                    </label>
+                    <div className="text-sm">
+                      <a href="#" className="font-semibold text-blue-200 hover:text-blue-500">
+                        Forgot password?
+                      </a>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="block w-full rounded-md p-2 border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-blue-300 placeholder:text-blue-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                
+              </form>
+              <br/>
+              <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-blue-200 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                    onClick={Signin}
+                  >
+                     sign in
+                  </button>
+                </div>
+              
+              <p className="mt-10 text-center text-sm text-blue-500">
+                wanna create acc?{' '}
+                <a href="signup" className="font-semibold leading-6 text-blue-200 hover:text-blue-500">
+                  Sign Up
+                </a>
+              </p>
+            </div>
+          </div>
+      ) : (
+        <div
+        className={`absolute w-full bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8`}
+      >
+        <div className="absolute ">
+          <center>
+          <img
+                className=" text-black-200 "
+                src="/images/heart.jpg"
+        
+              /></center>
+          </div>
+        <div className="fixed top-4 right-4 z-50">
+            <button
+              onClick={toggleMode}
+              className="px-4 py-2 bg-gray-300 rounded-md"
+            >
+              {darkMode ? 'Light Mode' : 'Dark Mode'}
+            </button>
+          </div>
+          <div className=" absolute top-10 right-13 sm:mx-auto sm:w-full sm:max-w-sm">
+          <Link href="/"><img
+              className=" text-blue-200 mx-auto h-10 w-auto"
+              src=""
+              alt="Your Company"
+            />
+            </Link>
+          </div>
+          
+          <div className=" relative bottom-20 sm:mx-auto sm:w-full sm:max-w-sm">
+            
+            <h2 className=" mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-black ">
+              Sign in  account
+            </h2>
+          </div>
+  
+          <div className="relative bottom-10 mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="email" className="block text-black text-sm font-medium leading-6 ">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="block w-full rounded-md p-2 border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-blue-200 placeholder:text-blue-200 focus:ring-2 focus:ring-inset focus:ring-blue-200 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-black">
+                    Password
+                  </label>
+                  <div className="text-sm">
+                    <a href="#" className="font-semibold text-black hover:text-blue-500">
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="block w-full rounded-md p-2 border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-blue-300 placeholder:text-blue-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              
+            </form>
+            <br/>
+            <div>
+                <button
+                  type="submit"
+                  className="flex bg-white w-full justify-center rounded-md text-black px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                  onClick={Signin}
+                >
+                   sign in
+                </button>
+              </div>
+            
+            <p className="mt-10 text-center text-sm text-black">
+              wanna create acc?{' '}
+              <a href="signup" className="font-semibold leading-6 text-black hover:text-blue-500">
+                Sign Up
+              </a>
+            </p>
+          </div>
+        </div>
+        
+        
+        )}
+        </>
+    );
+      };
